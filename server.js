935f6719a9bc4ee5041d74306f3dad4568909f75
Sergey Zelenov <zelenov@so1.ai>
2022-03-01 18:48:54 +0100

    use absolute path

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 const clients = new Set();
